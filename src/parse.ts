a3f621c13bbef8e22e38288f250dbc61a543e38e
KustelR <dmitriysstudios2@gmail.com>
2024-12-21 23:58:38 +0400

    saving children nodes now

--- src/parse.ts
+++ src/parse.ts
@@ -1,64 +1,111 @@
+import { ColoredNode, SizedNode } from "./node_types";
+import {
+  generateBgColor,
+  generateRounded,
+  generateTextColor,
+  generateSizes,
+} from "./generator_functions";
+
+type JSXConvertable = string | ParsedNode;
+
 enum classNameGenMods {
   Tailwind = 1,
 }
 
-function rgbToHex(color: {r: number, g: number, b: number}) {
-    function colorToHex(c: number) {
-        const cHex = Math.ceil(c * 255).toString(16);
-        return cHex.length !== 1 ? cHex : "0" + cHex;
-    }
-    return `#${colorToHex(color.r)}${colorToHex(color.g)}${colorToHex(color.b)}`;
-}
 
+type Padding = {top: number, bootom: number, left: number, right: number}
 type bgData = {
   fills: Array<Paint>;
 };
 
 class ParsedNode {
   constructor(node: BaseNode) {
+    this.type = node.type;
     this.name = node.name;
+
+    let sizedNode = node as SizedNode;
+    this.width = sizedNode.width;
+    this.height = sizedNode.height;
+
+    let textNode = node as TextNode;
+    this.text = textNode.characters;
+
     this.children = [];
-    const coloredNode: RectangleNode = node as RectangleNode;
-    if (coloredNode.fills !== undefined) {
-        if (typeof coloredNode.fills === "symbol") {
-        } else {
-            this.background = {
-                fills: [...coloredNode.fills]
-            }
-        }
+    if (node.type === "FRAME") {
+      node.children.forEach((child) => {
+        this.children.push(new ParsedNode(child));
+      });
+    } else if (node.type === "TEXT") {
+      this.children.push(this.text);
+    }
+    let coloredNode: ColoredNode = node as ColoredNode;
+    if (coloredNode.fills == undefined) this.paints = null;
+    if (typeof coloredNode.fills === "symbol") this.paints = null;
+    if (this.paints !== null) {
+      this.paints = {
+        /* TS expects that here can be passed symbol, but it checked above
+         */ //@ts-expect-error
+        fills: [...coloredNode.fills],
+      };
     }
   }
   getTag() {
+    switch (this.type) {
+      case "RECTANGLE":
+        return "div";
+      case "ELLIPSE":
+        return "div";
+      case "FRAME":
+        return "div";
+      case "TEXT":
+        if (this.text && this.text.length > 100) return "p";
+        else return "span";
+    }
     return "div";
   }
+
   getClassNames(mode: number): string {
     let output: string = "";
     if (mode === classNameGenMods.Tailwind) {
-        if (this.background && this.background.fills !== undefined && this.background.fills[0] !== undefined) {
-            const fill = this.background.fills[0];
-            if (fill.type === "SOLID") {
-
-                output += `bg-${rgbToHex(fill.color)} `;
-            }
-        }
+      if (this.paints && this.paints.fills !== undefined) {
+        if (this.type === "TEXT")
+          this.paints.fills.forEach(
+            (fill) => (output += generateTextColor(fill)),
+          );
+        else
+          this.paints.fills.forEach(
+            (fill) => (output += generateBgColor(fill)),
+          );
+      }
+      output += generateSizes(this);
+      if (this.type === "ELLIPSE") {
+        output += `${generateRounded(undefined, true)} `;
+      }
       return output;
     } else throw new Error("Wrong classNameGen mod");
   }
-  toJSX(): string {
-    return `<${this.getTag()} ` + 
-        `className="${this.getClassNames(classNameGenMods.Tailwind)}">` + 
-        `${this.children.map((child) => child.toJSX()).join("")}` +
-        `</${this.getTag()}>`;
-  }
+  type: string;
   name: string;
-  background?: bgData;
-  children: ParsedNode[];
+  width?: number;
+  height?: number;
+  text?: string;
+  paints?: bgData | null;
+  children: Array<JSXConvertable>;
 }
 
 function parseNode(node: BaseNode) {
   const serializedNode = new ParsedNode(node);
-    //console.log(serializedNode);
   return serializedNode;
 }
 
-export { parseNode, ParsedNode };
+function toJSX(node: JSXConvertable): string {
+  if (typeof node === "string") return node;
+  return (
+    `<${node.getTag()} ` +
+    `className="${node.getClassNames(classNameGenMods.Tailwind)}">` +
+    `${node.children.map((child) => toJSX(child)).join("")}` +
+    `</${node.getTag()}>\n`
+  );
+}
+
+export { parseNode, ParsedNode, toJSX };
