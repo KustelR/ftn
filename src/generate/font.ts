f26be80a18e1c294dabd0d01e8680b1cbb0ab027
KustelR <dmitriysstudios2@gmail.com>
2024-12-23 20:13:32 +0400

    prettier did it again

--- src/generate/font.ts
+++ src/generate/font.ts
@@ -1,22 +1,21 @@
 type TextedNode = {
-    fontName: {family: string, style: string} | symbol,
-    fontSize: number | symbol,
-    fontWeight: number | symbol,
-    textAlignHorizontal: 'LEFT' | 'CENTER' | 'RIGHT' | 'JUSTIFIED',
-    textAlignVertical: 'TOP' | 'BOTTOM' | 'CENTER',
-} 
+  fontName: { family: string; style: string } | symbol;
+  fontSize: number | symbol;
+  fontWeight: number | symbol;
+  textAlignHorizontal: "LEFT" | "CENTER" | "RIGHT" | "JUSTIFIED";
+  textAlignVertical: "TOP" | "BOTTOM" | "CENTER";
+};
 
 export default function generateFont(node: TextedNode): Array<string> {
-    const result: Array<string> = [];
-    //result.push(`break-words`)
-    if (typeof node.fontSize !== `symbol`) {
-    result.push(`text-[${node.fontSize}px]`)
-    }
-    if (typeof node.fontWeight !== `symbol`) {
-    result.push(`font-[${node.fontWeight}]`)
-    }
-    result.push(`text-${node.textAlignHorizontal.toLowerCase()}`)
+  const result: Array<string> = [];
+  //result.push(`break-words`)
+  if (typeof node.fontSize !== `symbol`) {
+    result.push(`text-[${node.fontSize}px]`);
+  }
+  if (typeof node.fontWeight !== `symbol`) {
+    result.push(`font-[${node.fontWeight}]`);
+  }
+  result.push(`text-${node.textAlignHorizontal.toLowerCase()}`);
 
-
-    return result;
-} 
+  return result;
+}
