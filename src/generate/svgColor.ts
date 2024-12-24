a6998335b1affda2703a52b6c808c1c28eacceed
KustelR <dmitriysstudios2@gmail.com>
2024-12-24 20:42:22 +0400

    generating svg data to render

---
+++ src/generate/svgColor.ts
@@ -0,0 +1,55 @@
+import rgbToHex from "../utils/rgbToHex";
+
+type SvgGradientData = {
+  type:
+    | "GRADIENT_LINEAR"
+    | "GRADIENT_RADIAL"
+    | "GRADIENT_DIAMOND"
+    | "GRADIENT_ANGULAR";
+  id: string;
+  stops: Array<{ id: string; color: RGBA; position: number }>;
+};
+export default function generateSVGFill(node: {
+  name: string;
+  fills: Array<Paint>;
+}): Array<string | SvgGradientData> {
+  const result: Array<string | SvgGradientData> = [];
+
+  if (node.fills.length === 0) {
+    result.push(`fillOpacity="0"`);
+  }
+
+  let i = 0;
+  node.fills.map((fill) => {
+    if (!fill.visible) return;
+
+    switch (fill.type) {
+      case "SOLID":
+        result.push(
+          `fill="${rgbToHex(fill.color)}"`,
+          fill.opacity != 1
+            ? `fillOpacity="${fill.opacity ? fill.opacity : 0}"`
+            : "",
+        );
+        break;
+      case "GRADIENT_LINEAR":
+        const gradient = fill as GradientPaint;
+        let j = 0;
+        result.push({
+          id: `lingrad${i}`,
+          type: gradient.type,
+          stops: gradient.gradientStops.map((stop) => {
+            j++;
+            return { id: `x${j}`, position: stop.position, color: stop.color };
+          }),
+        });
+        break;
+      default:
+        console.warn(
+          `Unsupported fill type (${fill.type}) for vector node: ${node.name}`,
+        );
+    }
+    i++;
+  });
+  return result;
+}
---
+++ src/generate/svgStroke.ts
@@ -0,0 +1,42 @@
+import rgbToHex from "../utils/rgbToHex";
+
+export default function generateSVGFill(
+  strokes: Array<Paint>,
+  node: {
+    name: string;
+    strokeWeight: number | symbol;
+    strokeRightWeight?: number;
+    strokeLeftWeight?: number;
+    strokeTopWeight?: number;
+    strokeBottomWeight?: number;
+    strokeALign?: "CENTER" | "INSIDE" | "OUTSIDE";
+    strokeCap:
+      | "NONE"
+      | "ROUND"
+      | "SQUARE"
+      | "ARROW_LINES"
+      | "ARROW_EQUILATERAL"
+      | symbol;
+  },
+): Array<string> {
+  const result: Array<string> = [];
+  strokes.map((stroke) => {
+    if (!stroke.visible) return;
+
+    switch (stroke.type) {
+      case "SOLID":
+        result.push(`stroke-[${rgbToHex(stroke.color)}]`);
+        break;
+      default:
+        console.warn(
+          `Unsupported stroke type (${stroke.type}) for vector node: ${node.name}`,
+        );
+    }
+  });
+
+  if (typeof node.strokeWeight !== "symbol" && node.strokeWeight >= 1) {
+    result.push(`stroke-[${node.strokeWeight}px]`);
+  }
+
+  return result;
+}
