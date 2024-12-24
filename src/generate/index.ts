ea7587de126b0154fa06d125199a329e1cb79977
KustelR <dmitriysstudios2@gmail.com>
2024-12-24 20:41:13 +0400

    exports for svg converters

--- src/generate/index.ts
+++ src/generate/index.ts
@@ -4,3 +4,5 @@ export { default as generateSpacing } from "./spacing";
 export { default as generateBorders } from "./borders";
 export { default as generateTextColor } from "./textColor";
 export { default as generateFont } from "./font";
+export { default as generateSvgColor } from "./svgColor";
+export { default as generateSvgStroke } from "./svgStroke";
