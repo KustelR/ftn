e99f6ec4d3ecbc00ca03c5e4ac2008c8119e5c2f
KustelR <dmitriysstudios2@gmail.com>
2024-12-27 00:20:08 +0400

    moved bordered node to typin file

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,6 +1,29 @@
 export {};
 declare global {
-  type TailwindProperties = Map<string, string | number>;
+  type BorderedNode = {
+    name: string;
+    width?: number;
+    height?: number;
+    cornerRadius?: number | symbol;
+    topLeftRadius?: number;
+    topRightRadius?: number;
+    bottomLeftRadius?: number;
+    bottomRightRadius?: number;
+    strokeWeight: number | symbol;
+    strokeRightWeight?: number;
+    strokeLeftWeight?: number;
+    strokeTopWeight?: number;
+    strokeBottomWeight?: number;
+    strokeALign?: "CENTER" | "INSIDE" | "OUTSIDE";
+    strokeCap?:
+      | "NONE"
+      | "ROUND"
+      | "SQUARE"
+      | "ARROW_LINES"
+      | "ARROW_EQUILATERAL"
+      | symbol;
+  };
+  type TailwindProperties = Map<string, string | number | boolean>;
   type ResultObject = {
     tagName: string;
     otherTags: Map<string, string>;
