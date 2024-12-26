d2f5bd16e20e54b34d693544e318ec0c62d66492
KustelR <dmitriysstudios2@gmail.com>
2024-12-27 00:18:57 +0400

    new bg color gen and tests for it

--- src/properties/bgColor.ts
+++ src/properties/bgColor.ts
@@ -1,12 +1,11 @@
 //import { ResultObject } from "@/types";
-import rgbToHex from "@/utils/rgbToHex";
+import generateTailwindColor from "@/utils/generateTailwindColor";
 //type HtmlData = Map<string, string | Array<string | TailwindProperty>>;
 
 export default function generateBgFromFills(node: {
   name: string;
   fills: Array<Paint>;
 }): TailwindProperties {
-  //let res: htmlData = new Map();
   const localClassNames: TailwindProperties = new Map();
   for (let i = 0; i < node.fills.length; i++) {
     const fill = node.fills[i];
@@ -17,17 +16,20 @@ export default function generateBgFromFills(node: {
       case "SOLID":
         localClassNames.set(
           "bg",
-          `[${rgbToHex(fill.color)}]/[${fill.opacity ? fill.opacity : 1}]`,
+          generateTailwindColor(fill.color, fill.opacity),
         );
         break;
       case "GRADIENT_LINEAR":
         localClassNames.set("bg-gradient-to", `l`);
         for (let j = 0; j < fill.gradientStops.length; j++) {
           const stop = fill.gradientStops[j];
+          const opacity = fill.opacity
+            ? stop.color.a * fill.opacity
+            : stop.color.a;
           if (j === 0) {
             localClassNames.set(
               "from",
-              `[${rgbToHex(stop.color)}]/[${stop.color.a * (fill.opacity ? fill.opacity : 1)}]`,
+              generateTailwindColor(stop.color, opacity),
             );
             if (stop.position !== 0)
               localClassNames.set(`from${j}`, `[${stop.position}]`);
@@ -35,7 +37,7 @@ export default function generateBgFromFills(node: {
           } else if (j === fill.gradientStops.length - 1) {
             localClassNames.set(
               `to`,
-              `[${rgbToHex(stop.color)}]/[${stop.color.a * (fill.opacity ? fill.opacity : 1)}]`,
+              generateTailwindColor(stop.color, opacity),
             );
             if (stop.position != 1)
               localClassNames.set(`to${j}`, `[${stop.position}]`);
@@ -43,7 +45,7 @@ export default function generateBgFromFills(node: {
           }
           localClassNames.set(
             `via${j}`,
-            `[${rgbToHex(stop.color)}]/[${stop.color.a * (fill.opacity ? fill.opacity : 1)}]`,
+            generateTailwindColor(stop.color, opacity),
           );
           localClassNames.set(`via${j}.${1}`, `[${stop.position}]`);
         }
---
+++ test/generateBgColor.test.ts
@@ -0,0 +1,169 @@
+import { describe, expect, test } from "@jest/globals";
+import { generateBgColor } from "@/properties";
+import generateBgFromFills from "@/properties/bgColor";
+
+const fillSolid: { name: string; fills: Array<Paint> } = {
+  name: "name",
+  fills: [
+    {
+      type: "SOLID",
+      color: { r: 1, g: 0, b: 0 },
+      visible: true,
+    },
+  ],
+};
+const fillSolidOpacity05: { name: string; fills: Array<Paint> } = {
+  name: "name",
+  fills: [
+    {
+      type: "SOLID",
+      color: { r: 1, g: 0, b: 0 },
+      visible: true,
+      opacity: 0.5,
+    },
+  ],
+};
+const fillSolidInvisible: { name: string; fills: Array<Paint> } = {
+  name: "name",
+  fills: [
+    {
+      type: "SOLID",
+      color: { r: 1, g: 0, b: 0 },
+      visible: false,
+    },
+  ],
+};
+
+describe("ignores invisible", () => {
+  test('should be undefined due to visible: false"', () => {
+    expect(generateBgColor(fillSolidInvisible).get("bg")).toBeUndefined();
+  });
+});
+
+describe("solid fill", () => {
+  test('should have item by key "bg"', () =>
+    expect(generateBgColor(fillSolid).get("bg")).toBeDefined());
+  test('bg should be [#ff0000]"', () =>
+    expect(generateBgColor(fillSolid).get("bg")).toEqual("[#ff0000]"));
+  test('bg should be [#ff0000]/[0.5]"', () =>
+    expect(generateBgColor(fillSolidOpacity05).get("bg")).toEqual(
+      "[#ff0000]/[0.5]",
+    ));
+});
+
+const gradientLinear: { name: string; fills: Array<Paint> } = {
+  name: "name",
+  fills: [
+    {
+      visible: true,
+      type: "GRADIENT_LINEAR",
+      gradientStops: [
+        {
+          position: 0,
+          color: { r: 0, g: 0, b: 0, a: 1 },
+        },
+        {
+          position: 0,
+          color: { r: 1, g: 1, b: 1, a: 1 },
+        },
+      ],
+      gradientTransform: [
+        [1, 0, 0],
+        [0, 1, 0],
+      ],
+    },
+  ],
+};
+
+const gradientLinearBig: { name: string; fills: Array<Paint> } = {
+  name: "name",
+  fills: [
+    {
+      visible: true,
+      type: "GRADIENT_LINEAR",
+      gradientStops: [
+        {
+          position: 0,
+          color: { r: 0, g: 0, b: 0, a: 1 },
+        },
+        {
+          position: 0.3,
+          color: { r: 1, g: 0, b: 0, a: 1 },
+        },
+        {
+          position: 0.6,
+          color: { r: 0, g: 1, b: 0, a: 1 },
+        },
+        {
+          position: 0,
+          color: { r: 1, g: 1, b: 1, a: 1 },
+        },
+      ],
+      gradientTransform: [
+        [1, 0, 0],
+        [0, 1, 0],
+      ],
+    },
+  ],
+};
+
+describe("simple linear gradient", () => {
+  test("should generate classes: bg-gradient-to-r", () => {
+    expect(generateBgFromFills(gradientLinear).get("bg-gradient-to")).toEqual(
+      "l",
+    );
+  });
+  test("should generate classes: from-[#000000]", () => {
+    expect(generateBgFromFills(gradientLinear).get("from")).toEqual(
+      "[#000000]",
+    );
+  });
+  test("should not generate classes: from-0", () => {
+    expect(generateBgFromFills(gradientLinear).get("from0")).toBeUndefined();
+  });
+  test("should generate classes: to-[#ffffff]", () => {
+    expect(generateBgFromFills(gradientLinear).get("to")).toEqual("[#ffffff]");
+  });
+  test("should not generate classes: to-1", () => {
+    expect(generateBgFromFills(gradientLinear).get("to2")).toBeUndefined();
+  });
+});
+
+describe("complicated linear gradient", () => {
+  test("should generate classes: from-[#000000]", () => {
+    expect(generateBgFromFills(gradientLinearBig).get("from")).toEqual(
+      "[#000000]",
+    );
+  });
+  test("should not generate classes: from-0", () => {
+    expect(generateBgFromFills(gradientLinearBig).get("from0")).toBeUndefined();
+  });
+  test("should generate classes: to-[#ffffff]", () => {
+    expect(generateBgFromFills(gradientLinearBig).get("to")).toEqual(
+      "[#ffffff]",
+    );
+  });
+  test("should not generate classes: to-1", () => {
+    expect(generateBgFromFills(gradientLinearBig).get("to4")).toBeUndefined();
+  });
+  test("should generate classes: via2-[#ff0000]", () => {
+    expect(generateBgFromFills(gradientLinearBig).get("via1")).toEqual(
+      "[#ff0000]",
+    );
+  });
+  test("should generate classes: via2.1-[0.3]", () => {
+    expect(generateBgFromFills(gradientLinearBig).get("via1.1")).toEqual(
+      "[0.3]",
+    );
+  });
+  test("should generate classes: via3-[#00ff00]", () => {
+    expect(generateBgFromFills(gradientLinearBig).get("via2")).toEqual(
+      "[#00ff00]",
+    );
+  });
+  test("should generate classes: via3.1-[0.6]", () => {
+    expect(generateBgFromFills(gradientLinearBig).get("via2.1")).toEqual(
+      "[0.6]",
+    );
+  });
+});
