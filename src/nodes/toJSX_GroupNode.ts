18858d8fdcacd5d5de0ca66569120793e60de157
KustelR <dmitriysstudios2@gmail.com>
2024-12-28 14:29:29 +0400

    ignore group nodes

--- src/nodes/toJSX_GroupNode.ts
+++ src/nodes/toJSX_GroupNode.ts
@@ -1,6 +1,11 @@
+import { generateLayout } from "@/properties";
 import { default as toJSX } from "@/toJsx";
+import generateTailwind from "@/utils/generateTailwind";
 
 export default function toJSX_GroupNode(node: GroupNode): string {
-  const children = node.children.map((child) => toJSX(child));
-  return `<div className="w-[${node.width}] h-[${node.height}] relative [&>*]:absolute">${children.join("")}</div>`;
+  return node.children
+    .map((child) => {
+      return toJSX(child);
+    })
+    .join("");
 }
