602c8a7bb6bdf6f08fcf7ae56f47ff99a20499c1
KustelR <dmitriysstudios2@gmail.com>
2024-12-29 15:01:24 +0400

    anomaly svg container sizing fixed

--- src/nodes/toJSX_FrameNodeVectors.ts
+++ src/nodes/toJSX_FrameNodeVectors.ts
@@ -1,11 +1,12 @@
 import { toJSX_VectorNode } from "@/nodes";
+import { generateBorders, generateSpacing } from "@/properties";
 import { generateDefs } from "@/properties/svg";
 import generateTailwind from "@/utils/generateTailwind";
 
 export default function toJSX_FrameNodeVectors(node: FrameNode): string {
-  const classNames: TailwindProperties = new Map();
-  classNames.set("relative", true);
-  classNames.set("[&>*]:absolute", true);
+  const className: Map<string, string> = new Map<string, string>();
+  className.set("max-w", `[${node.width}]`);
+  className.set("max-h", `[${node.height}]`);
   const defs: Set<SvgFill> = new Set();
   const children: string = node.children
     .map((child) => {
@@ -15,5 +16,5 @@ export default function toJSX_FrameNodeVectors(node: FrameNode): string {
       });
     })
     .join("");
-  return `<svg className="${generateTailwind(classNames)}" >${generateDefs(defs)}${children}</svg>`;
+  return `<svg className="${generateTailwind(className)}" >${generateDefs(defs)}${children}</svg>`;
 }
