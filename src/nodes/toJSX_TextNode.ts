b0b8b3162b49f04abe47cccc908343945bd4c39f
KustelR <dmitriysstudios2@gmail.com>
2024-12-29 15:02:44 +0400

    new property mapping support

--- src/nodes/toJSX_TextNode.ts
+++ src/nodes/toJSX_TextNode.ts
@@ -3,14 +3,14 @@ import { generateFont, generateLayout, generateTextColor } from "../properties";
 
 export default function toJSXWIP_TextNode(node: TextNode): string {
   let tagName: string = "span";
-  let classNames: Array<string> = [];
+  let classNames: TailwindProperties = new Map();
 
   if (typeof node.fills !== "symbol") {
-    classNames.push(
-      generateTextColor({ name: node.name, fills: [...node.fills] }).join(" "),
+    classNames = new Map(
+      generateTextColor({ name: node.name, fills: [...node.fills] }),
     );
   }
-  classNames.push(generateFont(node).join(" "));
-  classNames.push(generateTailwind(generateLayout(node)));
-  return `<${tagName} className="${classNames.join(" ")}">${node.characters}</${tagName}>`;
+  classNames = new Map([...generateFont(node), ...classNames]);
+  classNames = new Map([...generateLayout(node), ...classNames]);
+  return `<${tagName} className="${generateTailwind(classNames)}">${node.characters}</${tagName}>`;
 }
