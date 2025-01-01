4de06e3d63e69d0bf4b85668aa237b00caf50dfc
KustelR <dmitriysstudios2@gmail.com>
2025-01-01 14:35:47 +0400

    separated api call from main logic

--- src/index.ts
+++ src/index.ts
@@ -1,81 +1,53 @@
 import toJSX from "./toJsx";
 import { isConfig, generateConfig } from "@/utils/config";
 
+type UiMessageType = "GET_CODE_FROM_SELECTION" | "GET_LAST_CODE";
+type UiMessage = {
+  type: UiMessageType;
+  data: string;
+};
+
 if (figma.editorType === "figma") {
   let config: Config | undefined = await figma.clientStorage.getAsync("config");
   if (!isConfig(config)) {
     config = generateConfig();
     await figma.clientStorage.setAsync("config", config);
   }
+  let lastCode: string = "";
   figma.showUI(__html__);
   figma.ui.resize(600, 500);
-  figma.ui.onmessage = async (msg: { type: string; count: number }) => {
-    if (msg.type === "read-node") {
-      const selection = figma.currentPage.selection;
+  figma.ui.onmessage = async (msg: UiMessage) => {
+    switch (msg.type) {
+      case "GET_CODE_FROM_SELECTION":
+        lastCode = "";
+        const selection = figma.currentPage.selection;
+        lastCode = await getDataFromSelection([...selection], config);
 
-      selection.forEach(async (node) => {
-        const scNode = await figma.getNodeByIdAsync(node.id);
-        console.log(figma.currentPage.selection);
-        if (scNode) {
-          //figma.ui.postMessage(toJSX(parseNode(node)));
-          figma.ui.postMessage(toJSX(node, config));
-        }
-      });
+        figma.ui.postMessage(lastCode);
     }
-
     //figma.closePlugin();
   };
 }
 
 // Runs this code if the plugin is run in FigJam
 if (figma.editorType === "figjam") {
-  // This plugin will open a window to prompt the user to enter a number, and
-  // it will then create that many shapes and connectors on the screen.
+  console.log("doesn't support figjam yet");
+  figma.closePlugin();
+}
 
-  // This shows the HTML page in "ui.html".
-  figma.showUI(__html__);
-
-  // Calls to "parent.postMessage" from within the HTML page will trigger this
-  // callback. The callback will be passed the "pluginMessage" property of the
-  // posted message.
-  figma.ui.onmessage = (msg: { type: string; count: number }) => {
-    // One way of distinguishing between different types of messages sent from
-    // your HTML page is to use an object with a "type" property like this.
-    if (msg.type === "create-shapes") {
-      // This plugin creates shapes and connectors on the screen.
-      const numberOfShapes = msg.count;
-
-      const nodes: SceneNode[] = [];
-      for (let i = 0; i < numberOfShapes; i++) {
-        const shape = figma.createShapeWithText();
-        // You can set shapeType to one of: 'SQUARE' | 'ELLIPSE' | 'ROUNDED_RECTANGLE' | 'DIAMOND' | 'TRIANGLE_UP' | 'TRIANGLE_DOWN' | 'PARALLELOGRAM_RIGHT' | 'PARALLELOGRAM_LEFT'
-        shape.shapeType = "ROUNDED_RECTANGLE";
-        shape.x = i * (shape.width + 200);
-        shape.fills = [{ type: "SOLID", color: { r: 1, g: 0.5, b: 0 } }];
-        figma.currentPage.appendChild(shape);
-        nodes.push(shape);
-      }
-
-      for (let i = 0; i < numberOfShapes - 1; i++) {
-        const connector = figma.createConnector();
-        connector.strokeWeight = 8;
-
-        connector.connectorStart = {
-          endpointNodeId: nodes[i].id,
-          magnet: "AUTO",
-        };
-        connector.connectorEnd = {
-          endpointNodeId: nodes[i + 1].id,
-          magnet: "AUTO",
-        };
-      }
-
-      figma.currentPage.selection = nodes;
-      figma.viewport.scrollAndZoomIntoView(nodes);
-    }
-
-    // Make sure to close the plugin when you're done. Otherwise the plugin will
-    // keep running, which shows the cancel button at the bottom of the screen.
-    figma.closePlugin();
-  };
+async function getDataFromSelection(
+  selection: Array<SceneNode>,
+  config: Config,
+): Promise<string> {
+  let result = "";
+  let conversionPromises: Array<Promise<BaseNode | null>> = [];
+  selection.forEach((node) => {
+    conversionPromises.push(figma.getNodeByIdAsync(node.id));
+  });
+  const nodes = await Promise.all(conversionPromises);
+  nodes.forEach((node) => {
+    if (!node) return;
+    result += toJSX(node, config);
+  });
+  return result;
 }
