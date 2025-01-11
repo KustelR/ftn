d132f3300e1f357f07a24573cdf5a4ffe4489546
KustelR <dmitriysstudios2@gmail.com>
2025-01-11 19:32:55 +0400

    added classes to select

--- src/ui/scripts/input/select.ts
+++ src/ui/scripts/input/select.ts
@@ -1,12 +1,16 @@
+const selectClasses = ["bg-neutral-700"];
+
 export default function createSelect(
   name: string,
   selected: string,
   options: Array<string>,
 ): HTMLSelectElement {
   const select = document.createElement("select");
+  select.setAttribute("class", selectClasses.join(" "));
   select.name = name;
   options.forEach((option) => {
     const opt = document.createElement("option");
+    opt.textContent = option;
     if (selected === option) {
       opt.selected = true;
     }
