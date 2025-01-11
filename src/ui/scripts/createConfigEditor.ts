d248940ea5aa3413a4793c494b8b37182dc4bfcd
KustelR <dmitriysstudios2@gmail.com>
2025-01-11 19:29:59 +0400

    using select for output type

--- src/ui/scripts/createConfigEditor.ts
+++ src/ui/scripts/createConfigEditor.ts
@@ -1,11 +1,13 @@
 import { FromUiMessageType } from "@/types/FromUiType";
 import createGenericButton from "./buttons/createGenericButton";
+import createSelect from "@/ui/scripts/input/select";
 
 export default function createConfigEditor(config: Config): HTMLElement {
   let list = document.createElement("ul");
   list.setAttribute("class", "w-full space-y-2");
   const entries = Object.entries(config);
   entries.forEach((entry) => {
+    const propertyName = entry[0];
     const item = document.createElement("li");
     item.setAttribute(
       "class",
@@ -13,17 +15,27 @@ export default function createConfigEditor(config: Config): HTMLElement {
     );
     const configOptionName = document.createElement("h3");
     configOptionName.setAttribute("class", "col-span-3 text-lg font-bold");
-    configOptionName.textContent = entry[0];
-    const configOptionInput = document.createElement("input");
-    configOptionInput.setAttribute(
-      "class",
-      "col-span-5 min-w-10 justify-start items-start relative border-b-[2px] border-black bg-neutral-700",
-    );
-    try {
-      configOptionInput.setAttribute("value", JSON.stringify(entry[1]));
-    } catch (e) {
-      console.error("Error parsing JSON", e);
-      configOptionInput.setAttribute("value", entry[1] as OutputType);
+    configOptionName.textContent = propertyName;
+    let valueSource: HTMLSelectElement | HTMLInputElement;
+    if (propertyName === "outputType") {
+      const selected: string = entry[1] as OutputType;
+      const configSelect = createSelect(entry[0], selected, ["jsx", "html"]);
+      item.appendChild(configSelect);
+      valueSource = configSelect;
+    } else {
+      const configOptionInput = document.createElement("input");
+      configOptionInput.setAttribute(
+        "class",
+        "col-span-5 min-w-10 justify-start items-start relative border-b-[2px] border-black bg-neutral-700",
+      );
+      try {
+        configOptionInput.setAttribute("value", JSON.stringify(entry[1]));
+      } catch (e) {
+        console.error("Error parsing JSON", e);
+        configOptionInput.setAttribute("value", entry[1] as OutputType);
+      }
+      item.appendChild(configOptionInput);
+      valueSource = configOptionInput;
     }
     const configOptionButton = createGenericButton("set");
     configOptionButton.onclick = () => {
@@ -33,7 +45,7 @@ export default function createConfigEditor(config: Config): HTMLElement {
             type: FromUiMessageType.SET_CONFIG,
             data: JSON.stringify({
               property: configOptionName.textContent,
-              value: configOptionInput.value,
+              value: valueSource.value,
             }),
           },
         },
@@ -41,7 +53,6 @@ export default function createConfigEditor(config: Config): HTMLElement {
       );
     };
     item.appendChild(configOptionName);
-    item.appendChild(configOptionInput);
     item.appendChild(configOptionButton);
     list.appendChild(item);
   });
