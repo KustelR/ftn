74e47eb6e92d25ef2af23444184bb7fcf13a1e0c
KustelR <dmitriysstudios2@gmail.com>
2025-01-11 23:01:56 +0400

    recursively generating value from form

--- src/ui/scripts/input/form.ts
+++ src/ui/scripts/input/form.ts
@@ -25,9 +25,12 @@ type Json = {
 export function createJsonForm(
   itemTypes: Array<ItemString | ItemSelect>,
   obj: Json,
-): HTMLFormElement {
+): [HTMLFormElement, () => string] {
   const form = document.createElement("form");
   form.setAttribute("class", formClasses.join(" "));
+
+  const valueGenerators: Map<string, () => string> = new Map();
+
   itemTypes.forEach((entry) => {
     const itemContainer = document.createElement("div");
 
@@ -35,42 +38,70 @@ export function createJsonForm(
     label.textContent = entry.name;
     itemContainer.appendChild(label);
 
-    const child = createInteractiveFromItem(entry, obj);
+    const genResult = createInteractiveFromItem(entry, obj);
+    let child: HTMLElement;
+    let generator: () => string;
+    if (genResult) [child, generator] = genResult;
+    else throw new Error("can't generate form from json");
     if (child) itemContainer.appendChild(child);
+    valueGenerators.set(entry.name, generator);
 
     form.appendChild(itemContainer);
   });
-
-  return form;
+  return [
+    form,
+    () => {
+      const result: { [key: string]: string } = {};
+      valueGenerators.forEach((func, name) => {
+        result[name] = func();
+      });
+      return JSON.stringify(result);
+    },
+  ];
 }
-function createInteractiveFromItem(entry: ItemSelect | ItemString, obj: Json) {
+function createInteractiveFromItem(
+  entry: ItemSelect | ItemString,
+  obj: Json,
+): [HTMLElement, () => string] | undefined {
   let value = obj[entry.name];
   switch (entry.type) {
     case "string":
-      if (typeof value !== "string") return;
+      if (typeof value !== "string")
+        throw new Error("wrong string input type while generating json form");
       return createInputFromItem(entry.name, value);
     case "select":
       return createSelectFromItem(entry.name, entry.data, value);
     default:
-      console.warn("Unknown item type");
+      throw new Error("Unknown item type provided for form generator");
   }
 }
 function createInputFromItem(
   name: string,
   value: string | null,
-): HTMLInputElement {
+): [HTMLInputElement, () => string] {
   const input = createInput(undefined, name);
   if (value) input.value = value;
-  return input;
+  return [
+    input,
+    () => {
+      return input.value;
+    },
+  ];
 }
 
 function createSelectFromItem(
   name: string,
   value: Array<string> | null,
   selected: string | null,
-) {
+): [HTMLSelectElement, () => string] {
   if (!value) {
     throw new Error("Can't generate select: no options provided");
   }
-  return createSelect(name, value, selected ? selected : undefined);
+  const select = createSelect(name, value, selected ? selected : undefined);
+  return [
+    select,
+    () => {
+      return select.value;
+    },
+  ];
 }
