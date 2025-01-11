36a65a19fc82b39e398b823ac1f829366c188e50
KustelR <dmitriysstudios2@gmail.com>
2025-01-11 23:56:17 +0400

    remade it to just onclick handler

--- src/ui/scripts/buttons/buttonClear.ts
+++ src/ui/scripts/buttons/buttonClear.ts
@@ -1,11 +1,5 @@
-
-
-export default function addButtonClear() {
-const clear = document.getElementById("clear-code");
-if (!clear) throw new Error("Clear code button not found");
-clear.onclick = () => {
+export default function buttonClear() {
   const nodeData = document.getElementById("appRoot");
   if (!nodeData) throw new Error("nodeData was not found");
   nodeData.textContent = "";
-};
 }
