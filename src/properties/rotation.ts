267fb6aed6d0fcb9ae740077fc73f07ee24ce159
KustelR <dmitriysstudios2@gmail.com>
2025-01-14 22:38:15 +0400

    flattened

--- src/properties/rotation.ts
+++ src/properties/rotation.ts
@@ -1,18 +1,19 @@
 export default function getRotation(node: SceneNode): TailwindProperties {
   const result: TailwindProperties = new Map();
   if (
-    node.type !== "STICKY" &&
-    node.type !== "CODE_BLOCK" &&
-    node.type !== "WIDGET" &&
-    node.type !== "EMBED" &&
-    node.type !== "LINK_UNFURL" &&
-    node.type !== "MEDIA" &&
-    node.type !== "SECTION" &&
-    node.type !== "TABLE"
+    node.type === "STICKY" ||
+    node.type === "CODE_BLOCK" ||
+    node.type === "WIDGET" ||
+    node.type === "EMBED" ||
+    node.type === "LINK_UNFURL" ||
+    node.type === "MEDIA" ||
+    node.type === "SECTION" ||
+    node.type === "TABLE"
   ) {
-    if (node.rotation == 0) return result;
-    result.set("rotate", `[${-node.rotation}deg]`);
+    return result;
   }
+  if (node.rotation == 0) return result;
+  result.set("rotate", `[${-node.rotation}deg]`);
 
   return result;
 }
