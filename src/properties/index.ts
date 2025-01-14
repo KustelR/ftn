ff59fc1609eb4de83fcd7166dcf79021b40b9fb8
KustelR <dmitriysstudios2@gmail.com>
2025-01-14 21:50:08 +0400

    added rotation

--- src/properties/index.ts
+++ src/properties/index.ts
@@ -4,3 +4,4 @@ export { default as generateSpacing } from "./spacing";
 export { default as generateBorders } from "./borders";
 export { default as generateTextColor } from "./textColor";
 export { default as generateFont } from "./font";
+export { default as getRotation } from "./rotation";
