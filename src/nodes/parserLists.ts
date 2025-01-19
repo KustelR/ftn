597de32363e22c5cda540c5cfdc5a334b9104c61
KustelR <dmitriysstudios2@gmail.com>
2025-01-19 19:19:31 +0400

    added blacklists for new prop generators

---
+++ src/nodes/parserLists.ts
@@ -0,0 +1,24 @@
+const TextColorWhitelist = [];
+const BlendModeBlacklist = [
+  "SLICE",
+  "WIDGET",
+  "EMBED",
+  "LINK_UNFURL",
+  "MEDIA",
+  "SECTION",
+];
+const BackgroundColorBlacklist = [
+  "SLICE",
+  "GROUP",
+  "CONNECTOR",
+  "CODE_BLOCK",
+  "WIDGET",
+  "EMBED",
+  "LINK_UNFURL",
+  "MEDIA",
+  "SECTION",
+  "TEXT",
+  "VECTOR",
+];
+
+export { TextColorWhitelist, BlendModeBlacklist, BackgroundColorBlacklist };
