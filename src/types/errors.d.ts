90b60b7a56842cebfea5d54ad32cf1bd872ce5a9
KustelR <dmitriysstudios2@gmail.com>
2025-01-24 02:51:31 +0400

    added unsupported fill type error

--- src/types/errors.d.ts
+++ src/types/errors.d.ts
@@ -2,5 +2,6 @@ declare global {
   class FigmaMixedError extends Error {}
   class VectorPathsError extends Error {}
   class UnsupportedNodeTypeError extends Error {}
+  class UnsupportedFillTypeError extends Error {}
 }
 export {};
