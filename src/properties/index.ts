6e94caf1d2a6282c972173a6448756fae902a798
KustelR <dmitriysstudios2@gmail.com>
2025-01-24 02:54:48 +0400

    added deps

--- src/properties/index.ts
+++ src/properties/index.ts
@@ -1,8 +1,10 @@
 export { default as generateBgColor } from "./bgColor";
-export { default as generateLayout } from "./layout";
 export { default as generateSpacing } from "./spacing";
 export { default as generateBorders } from "./borders";
 export { default as generateTextColor } from "./textColor";
 export { default as generateFont } from "./font";
 export { default as getRotation } from "./rotation";
 export { default as getBlendMode } from "./blendMode";
+export { default as getAbsolutePosition } from "./position/getAbsolutePosition";
+export { default as getAutolayout } from "./position/getAutolayout";
+export { default as getNodeSize } from "./position/getNodeSize";
