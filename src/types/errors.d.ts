742c7acfeed2bd4e70ad6bbfc854c5be5f78088c
KustelR <dmitriysstudios2@gmail.com>
2025-01-27 01:23:26 +0400

    remade class exports

--- src/types/errors.d.ts
+++
@@ -1,7 +0,0 @@
-declare global {
-  export class FigmaMixedError extends Error {}
-  export class VectorPathsError extends Error {}
-  export class UnsupportedNodeTypeError extends Error {}
-  export class UnsupportedFillTypeError extends Error {}
-}
-export {};
