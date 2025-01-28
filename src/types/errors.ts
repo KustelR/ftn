53b92fcf877a4dd5c139c7019da7b77abd6f8232
KustelR <dmitriysstudios2@gmail.com>
2025-01-28 17:51:54 +0400

    added placeholder to unsupported node error

--- src/types/errors.ts
+++ src/types/errors.ts
@@ -1,4 +1,11 @@
 export class FigmaMixedError extends Error {}
 export class VectorPathsError extends Error {}
-export class UnsupportedNodeTypeError extends Error {}
+export class UnsupportedNodeTypeError extends Error {
+  constructor(message: string, options: ErrorOptions, placeholder: HtmlObject) {
+    super(message, options);
+    this.placeholder = placeholder;
+  }
+  placeholder: HtmlObject;
+}
 export class UnsupportedFillTypeError extends Error {}
+export class UnsupportedPropertyError extends Error {}
